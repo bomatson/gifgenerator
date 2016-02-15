d1b16df13f51b3fc7d15aeacab02fa57c168e0f8
bomatson <bobby@carbonfive.com>
2016-02-15 11:52:10 -0500

    remove js error

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,14 @@
 $(function() {
   var btn = document.querySelector('.copy-btn');
 
-  btn.addEventListener('click', function() {
-    var input = document.querySelector('.copy-input');
-    var text = input.setSelectionRange(0, input.value.length)
+  if(btn) {
+    btn.addEventListener('click', function() {
+      var input = document.querySelector('.copy-input');
+      var text = input.setSelectionRange(0, input.value.length)
 
-    var successful = document.execCommand('copy', true, text);
-    if(successful)
-      btn.textContent = 'Copied!'
-  });
+      var successful = document.execCommand('copy', true, text);
+      if(successful)
+        btn.textContent = 'Copied!'
+    });
+  }
 })
